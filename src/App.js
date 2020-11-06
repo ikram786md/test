acb2f1023aafa25c67927ef062b82c3da2b09d15
mdikram <ikram786md@gmail.com>
2020-11-06 17:33:48 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React  from 'react';
+
+class App extends React.Component{
+    render(){
+        return(
+        <div>Hi,I am Ikram   fan of PSPK
+         </div>
+         );
+    }
+}
+ export default App;
+
+
+
